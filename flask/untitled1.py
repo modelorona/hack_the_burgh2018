2d14c38aa99ed84e960a5ecd5f4cc25a735611b6
Anguel <angel.hristozov@gmail.com>
2018-03-11 11:26:20 +0000

    big move lol

--- flask/untitled1.py
+++ flask/untitled1.py
@@ -294,17 +294,71 @@ def url_check(myString):
 goodsites = ["https://www.economist.com/",
              "https://uk.reuters.com/"]
 
+import botometer
+
+# set up the botometer
+mashape_key = 'C0eUvCzWdDmshsbXAnK53fQRJUTtp1Km5x1jsnTQ2tMwIUruEp'
+twitter_app_auth = {
+    'consumer_key': 'rpmEbImgFwEHzOUFifA1peRcw',
+    'consumer_secret': 'agIECASC9skRiOVmanRVJO4XlDbTRk4vGU8a8lPBLl3OybkD6n',
+    'access_token': '972495546719010817-mjkyaHgxXExdrUFLfzOtt5F3K1cm0wJ',
+    'access_token_secret': '2ylFoxJ5W09JFTfACVtTzmzVOlUX48B1Uao4ktZccmOPQ'
+}
+
+
+# call this with the user url to check its scores
+def check_user(id=None, screen_name=None):
+    bom = botometer.Botometer(wait_on_ratelimit=True, mashape_key=mashape_key,**twitter_app_auth)
+    result = None
+    if id is not None:
+        # check by user id
+        result = bom.check_account(id)
+    elif screen_name is not None:
+        # check by screen name
+        result = bom.check_account(screen_name)
+    else:  # some nonsense with the params
+        return None
+
+    # content * 2 / 7
+    print("original:")
+    print(result)
+    print()
+    print("new average:")
+    avg = 0
+    for key, value in result['categories'].items():
+        if key == 'content':
+            avg += 2*value
+        avg += value
+    return ((avg/7))
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('twitterEntry.html')
+
+@app.route('/tbot', methods=['POST', 'GET'])
+def tbot():
+    id = request.values.get('name', None)
+    x = check_user(id)
+    if (x < 0.65):
+        return jsonify(result='Legit')
+    return jsonify(result='Bot')
+
+@app.route('/fbot', methods=['POST', 'GET'])
+def fbot():
+    id = request.values.get('name', None)
+    x = check_user(id)
+    if (x < 0.65):
+        return jsonify(result='Legit')
+    return jsonify(result='Bot')
 
 
 @app.route('/fnews', methods=['POST', 'GET'])
 def fnews():
-    print(request.values)
+    # print(request.values)
     url = request.values.get('url', None)
     overallScore = 0
+    print(url)
     content = get_content(url)
     if url in analyser():
         return jsonify(result='Fake')
