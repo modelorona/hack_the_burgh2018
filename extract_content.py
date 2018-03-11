c9fb42703c2abb8e3d9caffb0999d7dd0886c686
Anguel <angel.hristozov@gmail.com>
2018-03-11 08:40:12 +0000

    fixed

--- extract_content.py
+++ extract_content.py
@@ -1,10 +1,13 @@
+import requests
 from bs4 import BeautifulSoup
 
 
 def get_content(url):
-    soup = BeautifulSoup(url, "lxml")
+    url = requests.get(url)
+    soup = BeautifulSoup(url.content, "lxml")
     l = []
     for p in soup.find_all('p'):
         string = p.text.replace("\"", "\\\"").replace("\'", "\\'")
         l.append(string)
     return " ".join(l)
+
