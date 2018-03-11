4670b18cec7c0fce45d07e0712bba09283cedca0
Adam5i <czyzewski.i.a@gmail.com>
2018-03-11 04:44:22 +0000

    Working through it

--- ImageStuff/DecisionMaker.py
+++ ImageStuff/DecisionMaker.py
@@ -1,3 +1,16 @@
 import GettingData as G
+import scraper as s
 
-G.dataOnImage()
+def returnTheScore(LINK):
+    G.dataOnImage()
+    score = 0
+    for link in s.returnImageUrls(LINK)[::2]:
+        
+
+
+    return score
+
+
+
+print(returnImageUrls("https://www.facebook.com/motiwafashiondesinger/"))
+returnTheScore("https://www.facebook.com/motiwafashiondesinger/")
---
+++ ImageStuff/scraper.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+
+driver = webdriver.Chrome("./chromedriver_mac")
+
+def returnImageUrls(LINK):
+
+    tries = 0
+    driver.get(LINK+"photos/")
+    images = None
+    while(True):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        soup = BeautifulSoup(driver.page_source)
+        images = [img['src'] for img in soup.find('div',{'class':'_2eec'}).find_all('img')]
+        if(len(images)>50 and tries < 20):
+            break
+        time.sleep(2)
+        tries +=1
+
+    print(images)
+
+    driver.get(LINK)
+    posts = None
+
+    POSTS = []
+
+    while(True):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        soup = BeautifulSoup(driver.page_source)
+        posts = soup.find_all('div',{'class':'_q7o'})
+        if(len(posts)>30 and tries < 20):
+            for post in posts:
+                time_stamp = post.find("span",{"class":"timestampContent"})
+                content = post.find("div",{"class":"userContent"})
+                POSTS.append({"time_stamp":time_stamp.text,"content":content.text})
+            break
+
+
+    return POSTS
