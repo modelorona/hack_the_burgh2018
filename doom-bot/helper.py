1a22246ad8a6310ff5984115210f430989b14df2
hkdeman <2285702K@student.gla.ac.uk>
2018-03-11 05:57:38 +0000

    Add files via upload

--- doom-bot/helper.py
+++ doom-bot/helper.py
@@ -1,6 +1,7 @@
 import requests
 import math
 from enums import *
+import random
 
 HOST = "http://localhost:6001/api/"
 
@@ -38,16 +39,18 @@ def get_closest_enemy(my,others):
 def get_distance(my,other):
     return math.hypot(other['x'] - my['x'], other['y'] - my['y'])
 
-def turn_to_point(my_player,other_player, isPlayer = True):    
+def turn_to_point(my_player,other_player, isPlayer = True,is_visible=True):    
     MY_POS , POS  = my_player['position'], other_player['position']
-    rad_angle = math.atan2(MY_POS['y']-POS['y'],MY_POS['x']-POS['x'])
+    if not is_visible:    
+        rad_angle = math.atan2(MY_POS['y']-(POS['y']-650 if POS['y'] < 0 else POS['y'] + 650),MY_POS['x']-(POS['x']-650 if POS['x']<0 else POS['x']+650))        
+    else:    
+        rad_angle = math.atan2(MY_POS['y']-POS['y'],MY_POS['x']-POS['x'])
     deg_angle = rad2deg(rad_angle)
     norm_angle = deg_angle if deg_angle > 0 else deg_angle + 360
     remain = my_player['angle'] - norm_angle
     remain = remain if remain >=0 else remain + 360
 
     distance = get_distance(my_player['position'],other_player['position'])    
-    print(distance)
 
     if remain < 185 and remain > 175 and distance < 200 and isPlayer:
         shoot()
@@ -82,7 +85,28 @@ def update_players(my_player,other_player):
     other_player = get_player_details(other_player['id'])
     return my_player,other_player
 
-def decide_where_to_move(distance):
+CALLED = 0
+DISTANCE = []
+def is_still(distance):
+    global CALLED,DISTANCE
+    DISTANCE.append(distance)
+    CALLED+=1
+    if CALLED > 10:
+        average = sum(DISTANCE)/len(DISTANCE)
+        if abs(average - DISTANCE[0]) < 15:
+            CALLED, DISTANCE = 0,[]            
+            return True         
+        CALLED, DISTANCE = 0,[]
+        return False
+    else:
+        CALLED+=1
+        return False
+
+def decide_where_to_move(distance, is_visible = False):
+    # if not is_visible and is_still(distance):
+    #     move(Moves.BACKWARD,Amount.SAVAGE)
+    #     return
+
     if distance > 500:
         run_straight(Amount.SAVAGE)        
     elif distance > 300:
@@ -91,13 +115,20 @@ def decide_where_to_move(distance):
         run_straight(Amount.SLOW)
     else:
         run_backwards(Amount.SLOW)
-        
+
+def line_of_sight(my_player,other_player):
+    try:
+        is_visible = requests.get(HOST+"world/los/"+str(my_player['id'])+"/"+str(other_player['id'])).json()['los']
+        return is_visible         
+    except:
+        return False
 
 def move_to_enemy(my_player,other_player):
     distance = get_distance(my_player['position'],other_player['position'])
     while distance > 100 and not is_enemy_dead(other_player):
-        turn_to_point(my_player,other_player)
-        decide_where_to_move(distance)
+        is_visible = line_of_sight(my_player,other_player)        
+        turn_to_point(my_player,other_player,is_visible= is_visible)
+        decide_where_to_move(distance,is_visible=is_visible)
         my_player,other_player = update_players(my_player,other_player)        
         distance = get_distance(my_player['position'],other_player['position'])
 
@@ -110,18 +141,26 @@ def is_enemy_dead(player):
 def shoot():
     requests.post(HOST+"player/actions",json={"type":Moves.SHOOT,"amount":Amount.WEEBIT})    
 
-def go_grab_object(my_player,obj):
+def go_grab_object(my_player,obj,picking_weapon=True):
     distance = get_distance(my_player['position'],obj['position'])
     while distance > 30:
-        turn_to_point(my_player,obj,isPlayer=False)
+        is_visible = line_of_sight(my_player,obj)
+        turn_to_point(my_player,obj,isPlayer=False,is_visible=is_visible)
         run_straight(Amount.SAVAGE)
         my_player = get_my_player()
         distance = get_distance(my_player['position'],obj['position'])
     
-    if my_player['weapons'][obj['type']]:
+    if picking_weapon and my_player['weapons'][obj['type']]:
         return True
     else:
         return False
+
+    if not picking_weapon and my_player['ammo']['Shells']>10:
+        return True
+    else:
+        get_shells(my_player)
+        return False
+        
 
 def square_up(my_player):
     objects = requests.get(HOST+"world/objects").json()
@@ -133,3 +172,16 @@ def square_up(my_player):
     closest_obj = sorted(possible_pickups,key=lambda x: x['distance'])[0]
     return go_grab_object(my_player,closest_obj)
 
+def get_shells(my_player):
+    objects = requests.get(HOST+"world/objects").json()
+    possible_pickups = []
+    for obj in objects:
+        if obj['type']==AmmoType.SHOTGUN:
+            possible_pickups.append(obj)
+    
+    closest_obj = sorted(possible_pickups,key=lambda x: x['distance'])[0]
+    return go_grab_object(my_player,closest_obj,picking_weapon=False)
+
+def reload_ammo(my_player):
+    if my_player['ammo']['Shells']< 5:
+        get_shells(my_player)
--- doom-bot/runner.py
+++ doom-bot/runner.py
@@ -9,13 +9,17 @@ closest_enemy = get_closest_enemy(my_player,other_players)
 
 dead = False
 have_shotgun = False
+
+#Initial moves to prepare it
+move(Moves.RIGHT,Amount.MEDIUM)
+move(Moves.FORWARD,Amount.SAVAGE)
+
 while(not dead):
     if not have_shotgun:
         have_shotgun = square_up(my_player)
     move_to_enemy(my_player,closest_enemy)
-    # shoot()
     if is_enemy_dead(closest_enemy):
         other_players.remove(closest_enemy)
-        closest_enemy = get_closest_enemy(my_player,other_players)    
-        
-    
+
+    closest_enemy = get_closest_enemy(my_player,other_players)
+    reload_ammo(my_player)
