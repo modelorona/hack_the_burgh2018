4f9c6f2d24cc44ea8cd5d70a6a171bf4d88e5099
Adam5i <czyzewski.i.a@gmail.com>
2018-03-11 10:24:47 +0000

    A wee bit and it's gonna be done

--- ImageStuff/scraper.py
+++ ImageStuff/scraper.py
@@ -2,56 +2,55 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-driver = webdriver.Chrome("./chromedriver_mac")
+
 
 def returnImageUrls(LINK):
-
+    driver = webdriver.Chrome("./chromedriver_mac")
     tries = 0
     driver.get(LINK+"photos/")
     images = None
+    time.sleep(1)
     while(True):
+        time.sleep(0.7)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.2)
         soup = BeautifulSoup(driver.page_source)
-        time.sleep(5)
         images = [img['src'] for img in soup.find('div',{'class':'_2eec'}).find_all('img')]
         if(len(images)>50 and tries < 20):
             break
-        time.sleep(2)
         tries +=1
-
+    driver.close()
     return images
 
-    def returnUrlData(LINK):
+def returnUrlData(LINK):
+    driver = webdriver.Chrome("./chromedriver_mac")
+    tries = 0
+    driver.get(LINK + "photos/")
+    images = None
+    time.sleep(1)
+    while (True):
+        time.sleep(0.7)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.2)
+        soup = BeautifulSoup(driver.page_source)
+        images = [img['src'] for img in soup.find('div', {'class': '_2eec'}).find_all('img')]
+        if (len(images) > 50 and tries < 20):
+            break
+        tries += 1
 
-        tries = 0
-        driver.get(LINK + "photos/")
-        images = None
-        while (True):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            soup = BeautifulSoup(driver.page_source)
-            time.sleep(5)
-            images = [img['src'] for img in soup.find('div', {'class': '_2eec'}).find_all('img')]
-            if (len(images) > 50 and tries < 20):
-                break
-            time.sleep(2)
-            tries += 1
+    driver.get(LINK)
+    posts = None
 
-        return images
-
-        driver.get(LINK)
-        posts = None
-
-        POSTS = []
-
-        while(True):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            soup = BeautifulSoup(driver.page_source,"lxml")
-            posts = soup.find_all('div',{'class':'_q7o'})
-            if(len(posts)>30 and tries < 20):
-                for post in posts:
-                    time_stamp = post.find("span",{"class":"timestampContent"})
-                    content = post.find("div",{"class":"userContent"})
-                    POSTS.append({"time_stamp":time_stamp.text,"content":content.text})
-                break
-
-        return POSTS
+    POSTS = []
+    while(True):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        soup = BeautifulSoup(driver.page_source,"lxml")
+        posts = soup.find_all('div',{'class':'_q7o'})
+        if(len(posts)>30 and tries < 20):
+            for post in posts:
+                time_stamp = post.find("span",{"class":"timestampContent"})
+                content = post.find("div",{"class":"userContent"})
+                POSTS.append({"time_stamp":time_stamp.text,"content":content.text})
+            break
+    driver.close()
+    return POSTS
